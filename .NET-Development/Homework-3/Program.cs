6bf764d0187b34f96d90c69f18ba6ecd030f965e
Andrew <zaycraft07@gmail.com>
2023-05-04 00:33:25 +0300

    homework-3

---
+++ .NET-Development/Homework-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program : Program2
+{
+    public static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("Введіть число від якого потрібно почати зменшення");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть число до якого потрібно зменшувати");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Стовпчик усіх чисел від першого до другого включно: ");
+        while(a >= b) 
+        {
+            Console.WriteLine(a--);
+        }
+        sumDigits();
+    }
+}
---
+++ .NET-Development/Homework-3/Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program2
+{
+	public static void sumDigits()
+	{
+		int num, sum = 0;
+		Console.Write("Введіть число з якого потрібно знайти суму цифр: ");
+		num = Convert.ToInt32(Console.ReadLine());
+		int save = num;
+		while (num > 0)
+		{
+			sum += num % 10;
+			num /= 10;
+		}
+		Console.WriteLine($"Сума цифр числа {save} дорівнює {sum}");
+	}
+}
