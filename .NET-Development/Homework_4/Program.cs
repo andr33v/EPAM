41ca51fb6f6b6117c755b2f3edb189ee5082f315
Andrew <zaycraft07@gmail.com>
2023-05-14 00:08:21 +0300

    homework-4
    .NET - full done
    Front-End - full done
    Cloud - full done
    Overview - skipped

---
+++ .NET-Development/Homework_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program : Program2
+{
+    static void Main(string[] args)
+    {
+        //Підтримка українських символів
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        //Завдання #1
+        Console.WriteLine("Програма для обчислення суми додатних та кількості парних елементів: ");
+        int sum = 0;
+        int count = 0;
+
+        Console.Write("Введіть розмір масиву: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+
+        Console.WriteLine($"Введіть {size} чисел: ");
+        for (int i = 0; i < size; ++i)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for(int j = 0; j < size; ++j)
+        {
+            if (arr[j] > 0)
+            {
+                sum += arr[j];
+            }
+            if (arr[j] % 2 == 0)
+            {
+                ++count;
+            }
+        }
+
+        Console.WriteLine($"Сума додатних чисел введеного масиву: {sum} а кількість парних елементів: {count}");
+
+        //Перехід до задачі #2
+        Console.WriteLine();
+        Task2();
+    }
+}
---
+++ .NET-Development/Homework_4/Program2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program2 : Program3
+{
+    public static void Task2()
+    {
+        //Завдання #2
+        Console.WriteLine("Програма для обчислення суми максимального та мінімального елементів двовимірного масиву.");
+
+        Console.WriteLine("Введіть кількість рядків двовимірного масиву: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть кількість стовпчиків двовимірного масиву: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] arr = new int[rows, cols];
+
+        Console.WriteLine($"Введіть {rows * cols} чисел: ");
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                arr[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        int min = arr[0, 0];
+        int max = min;
+
+        foreach (int num in arr)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+            if (num < min)
+            {
+                min = num;
+            }
+        }
+
+        Console.WriteLine($"Мінімальне число цього масиву: {min} а максимальне: {max}, а їхня сума: {min + max}");
+        //Перехід до задачі #3
+        Console.WriteLine();
+        Task3();
+    }
+}
