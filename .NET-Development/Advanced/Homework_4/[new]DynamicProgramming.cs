bc223ab62694550223fa08c093f1cdb92bb63958
Andrew <zaycraft07@gmail.com>
2023-06-16 21:55:49 +0300

    dynamic programming task

--- .NET-Development/Advanced/Homework_4/[new]DynamicProgramming.cs
+++ .NET-Development/Advanced/Homework_4/[new]DynamicProgramming.cs
@@ -4,8 +4,14 @@ class NewDynamicProgramming
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Dynamic Programming - Number of combinations of 0 and 1, where 1 isn't repeated twice.");
         Console.Write("Enter a length of the number: ");
         int n = Convert.ToInt32(Console.ReadLine());
+
+        if(n == 0)
+        {
+            Console.WriteLine("There are no combinations at this length!");
+        }
 
         List<int> dp = new List<int>(5);
         dp.Add(2); // 0 and 1
@@ -16,6 +22,6 @@ class NewDynamicProgramming
             dp.Add(dp[i - 2] + dp[i - 1]);
         }
 
-        Console.WriteLine($"The number of combinations equals: {dp[n - 1]}");
+        Console.WriteLine($"The number of combinations at length {n} equals: {dp[n - 1]}");
     }
 }
